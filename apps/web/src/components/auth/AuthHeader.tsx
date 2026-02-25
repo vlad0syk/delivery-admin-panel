a3e1f0952a763607336724e0102e8c566e8b44a9
d3v_v0s <sapoznikvladislav58@gmail.com>
2026-02-25 23:11:44 +0200

    added minor changes to the front-end

--- apps/web/src/components/auth/AuthHeader.tsx
+++ apps/web/src/components/auth/AuthHeader.tsx
@@ -3,15 +3,13 @@ export default function AuthHeader() {
     <div className="flex flex-col items-center text-center m-4">
       <div className="mb-4 flex h-14 w-14 items-center justify-center rounded-full bg-indigo-100">
         <svg className="h-6 w-6 text-[#4d44f3]" stroke="currentColor" fill="currentColor">
-  <use href="/auth.svg#icon-envelope" />
-</svg>
+          <use href="/auth.svg#icon-envelope" />
+        </svg>
       </div>
 
       <h1 className="text-3xl font-semibold text-gray-900">
         Welcome to Admin Delivery Panel
       </h1>
-
-      
     </div>
   )
 }
