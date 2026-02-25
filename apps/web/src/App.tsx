1a32b7b67bfd7b87f1e10b94103ac9337a33341e
d3v_v0s <sapoznikvladislav58@gmail.com>
2026-02-25 23:24:55 +0200

    cleaned up unnecessary code

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import './app/auth/LoginPage'
 import LoginPage from './app/auth/LoginPage'
 
 function App() {
-  
-
   return (
     <>
-    <LoginPage />
+      <LoginPage />
     </>
   )
 }
