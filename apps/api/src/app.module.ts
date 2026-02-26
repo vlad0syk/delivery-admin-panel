829ac724ce32a9bfb2a74c7c5ad015d34a2500a7
Vitalii Foiak <vitaliy.foyak@gmail.com>
2026-02-26 21:32:01 +0200

    update

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,13 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { OrdersModule } from './orders/orders.module';
-
-@Module({
-  imports: [PrismaModule, OrdersModule],
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [PrismaModule, OrdersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
