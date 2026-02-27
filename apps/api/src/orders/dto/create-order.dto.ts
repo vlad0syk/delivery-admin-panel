0e28cb5d7e91409a85b71fd773b32aa48d5a2210
Vitalii Foiak <vitaliy.foyak@gmail.com>
2026-02-27 16:16:14 +0200

    fixed create-order dto

--- apps/api/src/orders/dto/create-order.dto.ts
+++ apps/api/src/orders/dto/create-order.dto.ts
@@ -1,7 +1,22 @@
+import { Type } from "class-transformer";
+import { IsNumber, IsString } from "class-validator";
+
 export class CreateOrderDto {
   id?: string;
+
+  @Type(() => Number)
+  @IsNumber()
   longitude!: number;
+
+  @Type(() => Number)
+  @IsNumber()
   latitude!: number;
+
+  @Type(() => String)
+  @IsString()
   timestamp!: string;
+
+  @Type(() => Number)
+  @IsNumber()
   subtotal!: number;
 }
