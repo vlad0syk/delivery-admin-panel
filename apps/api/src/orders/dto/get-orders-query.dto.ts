eb92104e4aebba065e81e159aced942e4f3234cc
max93139 <max1234btc@gmail.com>
2026-02-27 23:57:37 +0200

    fix pagination

--- apps/api/src/orders/dto/get-orders-query.dto.ts
+++ apps/api/src/orders/dto/get-orders-query.dto.ts
@@ -1,11 +1,39 @@
+import { IsOptional, IsString } from 'class-validator';
+
 export class GetOrdersQueryDto {
+  @IsOptional()
+  @IsString()
   page?: string;
+
+  @IsOptional()
+  @IsString()
   limit?: string;
+
+  @IsOptional()
+  @IsString()
   id?: string;
+
+  @IsOptional()
+  @IsString()
   taxRateRegionId?: string;
+
+  @IsOptional()
+  @IsString()
   taxRateRegionName?: string;
+
+  @IsOptional()
+  @IsString()
   dateFrom?: string;
+
+  @IsOptional()
+  @IsString()
   dateTo?: string;
+
+  @IsOptional()
+  @IsString()
   minSubtotal?: string;
+
+  @IsOptional()
+  @IsString()
   maxSubtotal?: string;
 }
